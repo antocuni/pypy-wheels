b257927071c4c141779b1e2bd534549291f6aca6
Antonio Cuni <anto.cuni@gmail.com>
2017-07-24 21:03:46 +0000

    I don't know why but 'yum install cmake28 git' gives me errors, so better to just remove them

--- docker/build_scripts/build.sh
+++ docker/build_scripts/build.sh
@@ -57,8 +57,8 @@ rpm -Uvh --replacepkgs epel-release-5*.rpm
 rm -f epel-release-5*.rpm
 
 # Development tools and libraries
-yum -y install bzip2 make git patch unzip bison yasm diffutils \
-    automake which file cmake28 \
+yum -y install bzip2 make patch unzip bison yasm diffutils \
+    automake which file  \
     kernel-devel-`uname -r` \
     devtoolset-2-binutils devtoolset-2-gcc \
     devtoolset-2-gcc-c++ devtoolset-2-gcc-gfortran \
