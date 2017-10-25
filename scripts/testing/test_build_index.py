d9a5d14a699bad06e59e4c938184816d970acbae
Antonio Cuni <anto.cuni@gmail.com>
2017-10-25 11:41:11 +0200

    start to write a test for build_index and, guess what? I found a bug. Make sure to list the package only once, even if the directory contains more wheels

--- scripts/testing/test_build_index.py
+++ scripts/testing/test_build_index.py
@@ -1,4 +1,5 @@
 import py
+import re
 from build_index import IndexBuilder
 
 def w(d, path, content):
@@ -6,6 +7,11 @@ def w(d, path, content):
     f.ensure()
     f.write(content)
     return f
+
+def find_links(f):
+    html = f.read()
+    matches = re.findall('<a href="(.*)">(.*)</a>', html)
+    return matches
 
 class TestIndexBuilder:
 
@@ -30,3 +36,18 @@ class TestIndexBuilder:
         assert dst.join('numpy/numpy-1.1-bla.whl').read() == 'existing 1.1'
         # note: this has NOT been copied, because it already existed
         assert dst.join('numpy/numpy-1.2-bla.whl').read() == 'existing 1.2'
+
+    def test_build_index(self, tmpdir):
+        src = tmpdir.join('src')
+        dst = tmpdir.join('dst')
+        w(src, 'netifaces/netifaces-10.1-bla.whl', 'AAA')
+        w(src, 'numpy/numpy-1.1-bla.whl', 'BBB')
+        w(src, 'numpy/numpy-1.2-bla.whl', 'CCC')
+        #
+        builder = IndexBuilder(src, dst)
+        builder.copy_wheels()
+        builder.build_index()
+        #
+        links = find_links(dst.join('index.html'))
+        assert sorted(links) == [('netifaces', 'netifaces'),
+                                 ('numpy', 'numpy')]
