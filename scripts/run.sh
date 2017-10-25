2656e4bd0fe4a4725a4ed4b6467af63b94c94314
Antonio Cuni <anto.cuni@gmail.com>
2017-10-25 11:10:16 +0200

    fix path

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 . docker/env.sh
 
 # build wheels on centos6
-#docker run -it --rm -v `pwd`:/pypy-wheels pypywheels/centos6:$TAG /pypy-wheels/build_wheels.sh
+#docker run -it --rm -v `pwd`:/pypy-wheels pypywheels/centos6:$TAG /pypy-wheels/scripts/build_wheels.sh
 
 # build wheels on ubuntu
-docker run -it --rm -v `pwd`:/pypy-wheels pypywheels/ubuntu14.04:$TAG /pypy-wheels/build_wheels.sh ubuntu
+docker run -it --rm -v `pwd`:/pypy-wheels pypywheels/ubuntu14.04:$TAG /pypy-wheels/scripts/build_wheels.sh ubuntu
