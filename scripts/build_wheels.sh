b85c5aa53e71643dc464cc0822e9dfef743977ec
Antonio Cuni <anto.cuni@gmail.com>
2017-10-25 15:56:38 +0200

    build wheels for all the pypy which are installed inside /opt/

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -2,7 +2,8 @@
 
 # this file is meant to be run inside the pypywheels docker image
 
-set -e -x
+#set -e -x
+set -e
 
 TARGET=$1
 TARGETDIR=/pypy-wheels/wheelhouse/$TARGETDIR
@@ -15,20 +16,23 @@ packages=(
     # pandas
 )
 
-# Compile wheels
+# Compile the wheels, for all pypys found inside /opt/
 echo "Compiling wheels..."
 echo
 cd
-
+for PYPY in /opt/pypy-*/bin/pypy
+do
+    echo "FOUND PYPY: $PYPY"
     # pip install using our own wheel repo: this ensures that we don't
     # recompile a package if the wheel is already available.
-    pypy -m pip install "${packages[@]}" \
-         --extra-index https://antocuni.github.io/pypy-wheels/$TARGET
+    $PYPY -m pip install "${packages[@]}" \
+          --extra-index https://antocuni.github.io/pypy-wheels/$TARGET
 
-    pypy -m pip wheel "${packages[@]}" \
-         -w wheelhouse \
-         --extra-index https://antocuni.github.io/pypy-wheels/$TARGET
-
+    $PYPY -m pip wheel "${packages[@]}" \
+          -w wheelhouse \
+          --extra-index https://antocuni.github.io/pypy-wheels/$TARGET
+    echo
+done
 
 # copy the wheels to the final directory
 mkdir -p $TARGETDIR
