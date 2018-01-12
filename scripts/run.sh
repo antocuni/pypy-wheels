c905dca280c7e746afdfa47e0238acb3950365b6
Antonio Cuni <anto.cuni@gmail.com>
2018-01-12 18:37:37 +0100

    something weird is happening here, let's try to print more

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,9 @@
 #docker run -it --rm -v `pwd`:/pypy-wheels pypywheels/centos6:$TAG /pypy-wheels/scripts/build_wheels.sh
 
 # build wheels on ubuntu
+echo "RUNNING DOCKER"
 docker run -e "PYPY" -it --rm -v `pwd`:/pypy-wheels pypywheels/ubuntu14.04:$TAG /pypy-wheels/scripts/build_wheels.sh ubuntu
+STATUS=$?
+echo "docker run exit status: $STATUS"
+echo "end of run.sh"
+exit $STATUS
