22cffe9abd8d2492a088601306d35b3ce842978a
Antonio Cuni <anto.cuni@gmail.com>
2019-11-11 18:24:50 +0100

    now that we merged the manylinux2010 branch, we can revert this hack

--- scripts/commit-gh-pages.sh
+++ scripts/commit-gh-pages.sh
@@ -47,18 +47,12 @@ TR_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
 echo "current git branch: $TR_BRANCH"
 if [ "$TR_BRANCH" == "master" ]
 then
-    echo "FIXME! REVERT THIS CHANGE AND PUSH ONLY IF MASTER!"
-    exit 1
     echo "pushing changes to $SSH_REPO"
     git push $SSH_REPO $TARGET_BRANCH
     EXIT_CODE=$?
 else
-    #echo "NOT pushing, since it's not master"
-    #EXIT_CODE=0
-
-    # remove me after merge to master
-    git push $SSH_REPO $TARGET_BRANCH
-    EXIT_CODE=$?
+    echo "NOT pushing, since it's not master"
+    EXIT_CODE=0
 fi
 
 # workaround for this travis bug:
